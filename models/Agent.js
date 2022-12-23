c0158086b1f283dad393c950e81fbc6f1e4c0a7c
LinusArdell <113325311+LinusArdell@users.noreply.github.com>
2022-12-23 20:25:29 +0700

    Agent HTML layout & add abilities routes

--- models/Agent.js
+++ models/Agent.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const AgentSchema = mongoose.Schema({
-    // Buat Schema data
+    // Buat Schema Agent
     displayName: {
         type: String,
         required: true
@@ -12,6 +12,62 @@ const AgentSchema = mongoose.Schema({
         required: true
     },
     displayIcon: {
+        type: String,
+        required: true
+    },
+    
+    // First Slot Abilty
+    firstAbilityName: {
+        type: String,
+        required: true
+    },
+    firstAbilityDescription:{
+        type: String,
+        required: true
+    },
+    firstAbilityIcon: {
+        type: String,
+        required: true
+    },
+        
+    // Second Slot ability
+    secondAbilityName: {
+        type: String,
+        required: true
+    },
+    secondAbilityDescription:{
+        type: String,
+        required: true
+    },
+    secondAbilityIcon: {
+        type: String,
+        required: true
+    },
+        
+    // Third ability
+    thirdAbilityName: {
+        type: String,
+        required: true
+    },
+    thirdAbilityDescription:{
+        type: String,
+        required: true
+    },
+    thirdAbilityIcon: {
+        type: String,
+        required: true
+    },
+        
+    // Ultimate
+    ultimate: {
+        type: String,
+        required: true
+    },
+    ultimateDescription: {
+        type: String,
+        required: true
+    },
+    ultimateIcon: {
         type: String,
         required: true
     }
--- routes/agent.js
+++ routes/agent.js
@@ -1,15 +1,37 @@
-// (5) Buat router Mahasiswa
+// (5) Buat router Agent
 const express = require('express')
 const router = express.Router()
 const Agent = require('../models/Agent')
 
 // Create 
 router.post('/', async (req, res) => {
-    // tampung input mahasiswa 
+    // tampung input Agent 
     const agentPost = new Agent({
+
+        // Agent's Identitiy
         displayName: req.body.displayName,
         description: req.body.description,
-        displayIcon: req.body.displayIcon
+        displayIcon: req.body.displayIcon,
+
+        // First slot ability
+        firstAbilityName: req.body.firstAbilityName,
+        firstAbilityDescription: req.body.firstAbilityDescription,
+        firstAbilityIcon: req.body.firstAbilityIcon,
+
+        // Second slot ability
+        secondAbilityName: req.body.secondAbilityName,
+        secondAbilityDescription: req.body.secondAbilityDescription,
+        secondAbilityIcon: req.body.secondAbilityIcon,
+
+        // Third slot ability
+        thirdAbilityName: req.body.thirdAbilityName,
+        thirdAbilityDescription: req.body.thirdAbilityDescription,
+        thirdAbilityIcon: req.body.thirdAbilityIcon,
+
+        // Ultimate
+        ultimate: req.body.ultimate,
+        ultimateDescription: req.body.ultimateDescription,
+        ultimateIcon: req.body.ultimateIcon
     })
 
     try {
